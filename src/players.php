3ba7f01cebb3b76048d01937156995fc5c4c1bae
mauricio gonzales <mauri9825@gmail.com>
2023-05-08 18:36:59 -0300

    Added Energia

--- src/players.php
+++ src/players.php
@@ -1,18 +1,40 @@
 <?php 
 
 class Jugador {
-    function __construct(protected $nombre, protected $numero)
+
+    protected $nombre;
+    protected $numero;
+    protected $energia = 100;
+
+    function __construct($nombre, $numero, $energia)
     {
-        
+        $this->nombre = $nombre;
+        $this->numero = $numero;
+        $this->energia = $energia;
     }
 
     public function getNombre(){
         return $this->nombre;
     }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function run ($distancia){
+        if ($distancia <= $this->energia * 10){
+            $this->energia -= ($distancia/10);
+            echo "Se corrio ", $distancia, " metros. Queda ", $this->energia, " energia";
+        }
+        else{
+            echo "No hay suficiente energia para correr ", $distancia, " metros";
+        }
+    }
 }
 
-//$messi = new Jugador("Messi", 10);
+$messi = new Jugador("Messi", 10, 100);
 
 //echo $messi->getNombre();
+$messi->run(15);
 
 ?>
